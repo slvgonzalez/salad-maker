9c2c9dd82ea3e98ef776d2758bac56654e473252
Silvia Gonzalez <slv.gonzalez.aller@gmail.com>
2022-05-14 17:04:53 +0200

    hardcoded size options

--- src/components/Salad/SaladForm.js
+++ src/components/Salad/SaladForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Form from '../../layouts/Form'
 import Input from '../../controls/Input'
+import Select from '../../controls/Select'
 import { Grid } from '@mui/material'
 
 
@@ -13,6 +14,15 @@ const SaladForm = () => {
             disabled
             name="saladName"
             label="Salad Name"
+          />
+          <Select
+            label="Size"
+            name="currentSize"
+            options = {[
+              { size: 'small', targetCost: 1.5, targetWeight: 150 },
+              { size: 'medium', targetCost: 2.5, targetWeight: 300 },
+              { size: 'large', targetCost: 3.5, targetWeight: 550 },
+            ]}
           />
         </Grid>
         <Grid item xs={6}>
