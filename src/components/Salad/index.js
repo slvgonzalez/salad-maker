a5d59c6fecae3c53acb092ecca1a46b0cf8f04ca
Silvia Gonzalez <slv.gonzalez.aller@gmail.com>
2022-05-16 15:51:13 +0200

    commented out ensaladas

--- src/components/Salad/index.js
+++ src/components/Salad/index.js
@@ -6,17 +6,43 @@ import SaladTable from './SaladTable';
 const Salad = (props) => {
   console.log(props)
 
-  const list = props.list
+  const allSalads = props.list
 
-  const displaySalads = (list) => {
+  // const calculateCost = () => {
+  //   console.log("calculamos coste")
+  // }
 
+  // const calculatePrice = () => {
+  //   console.log("calculamos precio")
+  // }
+
+  // const calculateHoursFresh = () => {
+  //   console.log("calculamos aguante")
+  // }
+
+  // const ensaladas = allSalads.map((ensalada) => {
+  //   return (
+      // id: {ensalada.id},
+      // name: {ensalada.name},
+      // type: {ensalada.type},
+      // ingredients: {ensalada.ingredients},
+      // cost: {calculateCost()},
+      // price: {calculatePrice()},
+      // hoursFresh: {calculateHoursFresh()}
+  //   )
+  // })
+
+  console.log("aqui las ensaladas")
+  // console.log(ensaladas)
+
+  const displaySalads = (allSalads) => {
     const types = props.types
     const ingredients = props.products
     return (
-      list.map((salad) => (
+      allSalads.map((salad) => (
         <div className='wrapper' key={salad.id}>
           <SaladTable  salad={salad} ingredients={ ingredients } types={ types }/>
-          <Button variant="text" onClick={console.log("add ingrdient")}>ADD INGREDIENT</Button>
+          <Button variant="text" onClick={() => console.log("add ingrdient")}>ADD INGREDIENT</Button>
         </div >
       ))
     )
@@ -24,7 +50,7 @@ const Salad = (props) => {
 
   return (
     <>
-      { displaySalads(list) }
+      { displaySalads(allSalads) }
     </>
   )
 }
